c792d3ab3b43ead095c56f13cb33d26568a2c2ad
WawanC <kurniawancristianto@gmail.com>
2021-09-22 18:19:11 +0700

    Connecting to DB

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,7 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
+import mongoose from "mongoose";
 
 import corsHandler from "./utils/cors";
 import todoRoutes from "./routes/todo";
@@ -9,4 +12,14 @@ app.use(express.json());
 app.use(corsHandler);
 app.use(todoRoutes);
 
-app.listen(3000);
+const runApp = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI as string);
+    console.log("Connected to DB");
+    app.listen(3000);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+runApp();
